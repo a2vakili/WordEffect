4824561aefdcc62e1ec1336fd0636ef58cba8570
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-08-04 14:38:39 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Arsalan Vakili on 2015-08-04.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
